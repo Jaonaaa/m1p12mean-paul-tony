99515f2b086b0e818e6d5c4c1480e57e6d957915
Jaonaaa <jaonapaulferdinah@gmail.com>
2025-03-18 19:16:24 +0300

    feat: sending email when the user sign-up, using mailersend

---
+++ services/email/index.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
+
+dotenv.config();
+
+const { MAILERSENDER_API_KEY, MAILERSENDER_SENDER, TEMPLATE_ID } = process.env;
+
+const mailerSend = new MailerSend({
+  apiKey: MAILERSENDER_API_KEY,
+});
+
+const sentFrom = new Sender(MAILERSENDER_SENDER, "Vroom");
+
+export const sendEmail = async (email, name) => {
+  try {
+    const recipients = [new Recipient(email, name)];
+    const personalization = [
+      {
+        email: email,
+        data: {
+          name: name,
+        },
+      },
+    ];
+    const emailParams = new EmailParams()
+      .setFrom(sentFrom)
+      .setTo(recipients)
+      .setReplyTo(sentFrom)
+      .setTemplateId(TEMPLATE_ID)
+      .setPersonalization(personalization);
+
+    await mailerSend.email.send(emailParams);
+  } catch (error) {
+    throw error;
+  }
+};
