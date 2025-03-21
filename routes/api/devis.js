022bd0073bd73f6ee3c24180acdafd71aaaa2911
Jaonaaa <jaonapaulferdinah@gmail.com>
2025-03-21 15:20:04 +0300

    updating table

--- routes/api/devis.js
+++ routes/api/devis.js
@@ -1,32 +1,26 @@
 import { Router } from "express";
 import Devis from "../../models/Devis.js";
 import Response, { Status } from "../../models/app/Response.js";
+import createDevis from "../../services/api/devis/client.js";
 
 const devisRouter = Router();
 
-devisRouter.post("/", async (req, res) => {
-  const devis = new Devis(req.body);
-  await devis.save();
-  res.status(201).json(new Response("", Status.Ok, devis));
+devisRouter.post("/", async (req, res, next) => {
+  try {
+    const newDevis = await createDevis(req.body);
+    res.status(201).json(new Response("Devis créer avec succes", Status.Ok, newDevis));
+  } catch (error) {
+    next(error);
+  }
 });
 
-devisRouter.get("/", async (req, res) => {
-  const devis = await Devis.find();
-  res.status(200).json(new Response("", Status.Ok, devis));
+devisRouter.get("/", async (req, res, next) => {
+  try {
+    const devis = await Devis.find();
+    res.status(200).json(new Response("", Status.Ok, devis));
+  } catch (error) {
+    next(error);
+  }
 });
-
-devisRouter.put("/:id", async (req, res) => {
-  const devis = await Devis.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.status(201).json(new Response("", Status.Ok, devis));
-});
-
-devisRouter.delete("/:id", async (req, res) => {
-  await Devis.findByIdAndDelete(req.params.id);
-  res.status(201).json(new Response("Devis supprimé", Status.Ok));
-});
-
-export const findDevis = async (id) => {
-  return await Devis.findById(id).populate("services").populate("id_client");
-};
 
 export default devisRouter;
