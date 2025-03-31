93ddc8bc93a6da8d3adef73dd24bb5c17b09263f
Jaonaaa <jaonapaulferdinah@gmail.com>
2025-03-31 21:09:51 +0300

    feat: API for getting compatible emp for a task

---
+++ services/api/task/employe.js
@@ -0,0 +1,20 @@
+import MyError from "../../../models/app/MyError.js";
+import Employe from "../../../models/Employe.js";
+import Services_details_in_devis from "../../../models/Services_details_in_devis.js";
+import { formatUser } from "../../auth/user.js";
+import { formatEmployes } from "../employe/index.js";
+
+const MESSAGES = {
+  TASK_NOT_FOUND: "Tâche introuvable",
+  NO_COMPATIBLE_EMPLOYEES: "Aucun employé compatible pour cette tâche",
+};
+export const getCompatibleEmp = async (id_task) => {
+  const task = await Services_details_in_devis.findById(id_task).populate("service");
+  if (!task) throw new MyError(MESSAGES.TASK_NOT_FOUND);
+  const service = task.service;
+  let emp = [];
+  if (service.required_skills.length == 0) emp = await Employe.find().populate(["id_user", "skills"]);
+  else emp = await Employe.find({ skills: { $all: service.required_skills } }).populate(["id_user", "skills"]);
+  emp = formatEmployes(emp);
+  return emp;
+};
