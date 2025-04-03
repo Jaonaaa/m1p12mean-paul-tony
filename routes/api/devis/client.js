9de23b619fa44fa41c15040c4dee0f283d5fce37
Jaonaaa <jaonapaulferdinah@gmail.com>
2025-04-03 22:39:54 +0300

    testing on prod

--- routes/api/devis/client.js
+++ routes/api/devis/client.js
@@ -33,8 +33,7 @@ devisClientRouter.get("/details/:id_devis", async (req, res, next) => {
 
     let devis = await Devis.findById(id_devis).populate(devisPopulateAll);
     if (!devis) throw new MyError(MESSAGES.DEVIS_NOT_FOUND, 404);
-
-    console.log(devis.services_details.workers);
+    if (devis.services_details.length > 0) console.log(devis.services_details[0].workers);
     devis.services_details = devis.services_details.map((service) =>
       service.workers.map((worker) => {
         worker.id_user = formatUser(worker.id_user);
@@ -42,10 +41,10 @@ devisClientRouter.get("/details/:id_devis", async (req, res, next) => {
       })
     );
     console.log("====================================");
-    console.log(devis.services_details.workers);
+    if (devis.services_details.length > 0) console.log(devis.services_details[0].workers);
     console.log("====================================");
     let work = await Work.findOne({ id_devis: devis._id });
-    devis = formatClientInDevis([devis])[0];
+    if (devis.services_details.length > 0) console.log(devis.services_details[0].workers);
     console.log(devis.services_details.workers);
     res.status(200).json(new Response("", Status.Ok, { devis: devis, detail: work }));
   } catch (error) {
